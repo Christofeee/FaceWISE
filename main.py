8c964965e0b6147453656900038b4da3c0c4e377
Christofeee <117144492+Christofeee@users.noreply.github.com>
2023-09-16 15:41:48 +0630

    input image from cloud test1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cv2
 import numpy as np
+from PIL import Image 
 
 # Load pretrained models for age and gender detection
 # ...
@@ -11,15 +12,16 @@ uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"]
 
 if uploaded_image:
     # image = cv2.imread(uploaded_image)
-    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    #image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    image = Image.open(uploaded_image)
     # Perform age and gender detection on the uploaded image
     # ...
 
     # Convert BGR image to RGB
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # Display the image with predictions
-    st.image(image_rgb, caption="Predicted Age and Gender", use_column_width=True)
+    st.image(image, caption="Predicted Age and Gender", use_column_width=True)
 
     # Display age and gender predictions
     st.write(f"Predicted Age: ...")
