e76369cc04852a9b1f22276d0707d73b7d8a4335
Christofeee <117144492+Christofeee@users.noreply.github.com>
2023-09-16 15:07:49 +0630

    created streamlit and added code for Image input and display

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+# Load pretrained models for age and gender detection
+# ...
+
+# Create Streamlit UI
+st.title("Age and Gender Detection")
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_image:
+    # image = cv2.imread(uploaded_image)
+    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    # Perform age and gender detection on the uploaded image
+    # ...
+
+    # Convert BGR image to RGB
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Display the image with predictions
+    st.image(image_rgb, caption="Predicted Age and Gender", use_column_width=True)
+
+    # Display age and gender predictions
+    st.write(f"Predicted Age: ...")
+    st.write(f"Predicted Gender: ...")
